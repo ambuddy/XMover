d218bd955c651c0c09a5aec0fce19cf329e47617
Nikolay <ambuddy@mail.ru>
2017-11-22 15:43:08 +0300

    Update XMover.js

--- XMover.js
+++ XMover.js
@@ -5,13 +5,13 @@
  *	and view how its co0rdinates change in browser console.
  *
  *	Just add some objects to xmover in any part of code as follows:
- *  xmover.add(anyDisplayObject);
- *  xmover.add(myTextField, myPIXIContainer ... mySprite);
- *  xmover.add([myObj1, myObj2 ... myObjN]);
+ *	xmover.add(anyDisplayObject);
+ *	xmover.add(myTextField, myPIXIContainer ... mySprite);
+ *	xmover.add([myObj1, myObj2 ... myObjN]);
  *	You may add as many objects as you want as many times as desired passing one object,
  *	or a bunch of comma separated objects or an array of objects.
  *
- *  Then you can move added objects one by one on the scene using arrow keys. Also you
+ *	Then you can move added objects one by one on the scene using arrow keys. Also you
  *	can rotate them using <> keys, hide and reveal using sqr bracket ([]) keys.
  *	Switching between objects performed by -+ keys. Once you switched to next object
  *	it jumps and blinks once.
